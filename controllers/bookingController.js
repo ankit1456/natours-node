091c864ed5d0943f26c5a51727a73dee8e2b5c8e
ankittripathi <ankittripathi1456@gmail.com>
2023-03-11 14:06:22 +0530

    log: added a log

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,7 +55,9 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error : ${err.message}`);
   }
 
+  console.log(event);
   if (event.type === 'checkout.session.completed') {
+    console.log(event);
     createBooking(event.data.object);
 
     res.status(200).json({
